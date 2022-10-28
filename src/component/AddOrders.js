bf421c3679069dc4512b2cfbadb33250cbbcb61f
minhquangle <tuilaminhquang970>
2022-10-29 01:21:57 +0700

    Final Payment

--- src/component/AddOrders.js
+++ src/component/AddOrders.js
@@ -1,30 +1,28 @@
-import React, { useState, useRef } from "react";
-import { Button, FormControl, InputGroup, Form, Container, Toast, Alert, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, FormControl, InputGroup, Form, Container, Toast } from "react-bootstrap";
 import toast, { Toaster } from "react-hot-toast";
 import { authApi, endpoints } from "../configs/Api";
 import { useStateContext } from "../reducer/StateContext";
 import Category from "./Category";
-import Payment from "./Payment";
+import { PayPalButtons } from '@paypal/react-paypal-js'
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 
 export default function AddOrder() {
-    const [address, setAddress] = useState('')
+    const [address, setAddress] = useState()
     const { cartItems, setCartItems, setTotalQuantities, setTotalPrice, totalPrice } = useStateContext()
-    const price = (parseFloat(totalPrice) / 24000).toFixed(2)
 
-
-    const AddReceipt = () => {
-
+    const Addorders = () => {
+        const place = document.getElementById('address').value
         let AddOder = async () => {
             const formData = new FormData()
-            formData.append("ship_address", address)
+            formData.append("ship_address", place)
             try {
                 const res = await authApi().post(endpoints['addorder'], formData, {
                     headers: {
                         "Content-Type": "multipart/form-data"
                     }
                 })
-                console.log(res)
 
                 cartItems.map(c => {
 
@@ -49,7 +47,6 @@ export default function AddOrder() {
             } catch (err) {
                 console.error(err)
             }
-            setAddress('')
             setCartItems([])
             setTotalQuantities(0)
             setTotalPrice(0)
@@ -59,54 +56,72 @@ export default function AddOrder() {
 
         AddOder()
 
+
+
     }
+    const handleApprove = () => {
 
+        Addorders()
+        toast.success('Successfully!')
 
-
-
-
+    }
     let body = <>
-        <Container>
-            <Alert variant="danger">
-                Chưa có đơn hàng
-            </Alert>
-
-        </Container>
-
+        <h1 style={{ textAlign: 'center' }} >Chua co don hang</h1>
 
 
 
     </>
-
-
     if (cartItems.length > 0) {
         body = <>
 
             <Container>
-                <div>
+                <div style={{ margin: '5px', display: "flex", width: '100%', flexWrap: 'wrap', }} >
                     {cartItems.map(c => {
                         return <Category id={c.id} image={c['image']} name={c['name']} price={c.price} title={c.title} quantity={c.quantity} />
                     })}
                 </div>
-                <Row>
-                    <Col>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Địa chỉ giao hàng: </Form.Label>
-                            <Form.Control
-                                type="text"
-                                value={address}
-                                onChange={(event) => {
-                                    setAddress(event.target.value)
-                                }}
-                            />
-                        </Form.Group></Col>
-                    <Col></Col>
-                </Row>
+                <div className="add-address"  >
+                    <InputGroup size="sm" className="mb-3" style={{ margin: "10px", width: "80%" }}>
+                        <InputGroup.Text id="inputGroup-sizing-sm">Nhap Dia Chi</InputGroup.Text>
+                        <Form.Control
+                            id="address"
+                            aria-label="Small"
+                            aria-describedby="inputGroup-sizing-sm"
+                            type="text"
+                            value={address}
+                            onChange={(event) => setAddress(event.target.value)}
+                        />
+                    </InputGroup>
 
 
-                <Payment total={price} AddReceipt={AddReceipt} />
+                </div>
+                <PayPalButtons
+                    createOrder={(data, actions) => {
+                        return actions.order.create({
+
+                            purchase_units: [
+                                {
+                                    description: 'Payment',
+                                    amount: {
+                                        value: (parseFloat(totalPrice) / 24000).toFixed(2)
 
 
+                                    }
+                                }
+                            ]
+                        })
+                    }}
+                    onApprove={async (data, actions) => {
+                        const order = await actions.order.capture();
+                        console.log("order: ", order)
+                        handleApprove()
+                    }}
+                    onError={(err) => {
+                        console.log("Error: ", err)
+                    }
+
+                    }
+                />
 
 
             </Container>
@@ -115,14 +130,14 @@ export default function AddOrder() {
     }
 
 
-
     return (
         <>
-
-            {body}
-            <Toaster />
-
+            <PayPalScriptProvider options={{
+                "client-id": "ARPZplyh7bDL43TQSiqTF3Tf7ytbjE5vNvZsUzrPfLunZfbioCY2KNWrQIVzgI08NOvJW2xTNSi5GOhi"
+            }}>
+                {body}
+                <Toaster />
+            </PayPalScriptProvider>
         </>
     )
 }
-
